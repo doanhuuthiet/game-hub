95341b1c12131ebe7c04a0a0de3087c5f3ffa696
thiet doan <doanhuuthiet@gmail.com>
2023-11-29 13:48:48 +0700

    test

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,13 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
-  return <HStack></HStack>;
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px"></Image>
+      <Text>NavBar</Text>
+    </HStack>
+  );
 };
 
 export default NavBar;
